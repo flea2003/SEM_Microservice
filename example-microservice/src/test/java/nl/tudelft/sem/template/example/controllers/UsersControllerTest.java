3bd2bd2b54bcc5a3f9e80156d6d9f9ef04ce2a54
Gabriel Tertelici <G.Tertelici@student.tudelft.nl>
2023-12-05 20:30:52 +0100

    Tested Endpoint

--- example-microservice/src/test/java/nl/tudelft/sem/template/example/controllers/UsersControllerTest.java
+++ example-microservice/src/test/java/nl/tudelft/sem/template/example/controllers/UsersControllerTest.java
@@ -2,53 +2,81 @@ package nl.tudelft.sem.template.example.controllers;
 
 import nl.tudelft.sem.template.example.domain.user.InvalidUserException;
 import nl.tudelft.sem.template.example.domain.user.RegistrationService;
-import nl.tudelft.sem.template.example.domain.user.UserRepository;
+import nl.tudelft.sem.template.example.domain.user.User;
 import nl.tudelft.sem.template.example.models.UserPostRequest;
-import nl.tudelft.sem.template.example.profiles.TestUserRepository;
 
-import static org.mockito.Mockito.doReturn;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest
-@ExtendWith(SpringExtension.class)
-@AutoConfigureMockMvc
 class UsersControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
 
     private static RegistrationService registrationService;
+    private static UsersController sut;
     @BeforeAll
     static void setup() throws Exception {
         registrationService = Mockito.mock(RegistrationService.class);
-
+        sut = new UsersController(registrationService);
         //Invalid input registration
-        when(registrationService.registerUser("!user","email@google.com","pass123")).thenThrow(new InvalidUserException());
+        when(registrationService.registerUser("!user","email@gmail.com","pass123")).thenThrow(new InvalidUserException());
+
+        //Valid user -> return user object
+        User added = new User("user","email@gmail.com","pass123");
+        added.setId(1);
+        when(registrationService.registerUser("user","email@gmail.com","pass123")).thenReturn(added);
+
+        //Same email exists twice
+        when(registrationService.getUserByEmail("iexisttwice@gmail.com")).thenReturn(added);
+
+        //Fake a database insertion failed
+        when(registrationService.registerUser("userImpossible","email@gmail.com","pass123")).thenThrow(new Exception("Database went boom"));
     }
     @Test
-    void registerEmptyInput() throws Exception{
-        UserPostRequest userToAdd = new UserPostRequest("!user","email@google.com","");
+    void registerEmptyInput(){
+        UserPostRequest userToAdd = new UserPostRequest("user","email@gmail.com","");
 
-        ResultActions result = mockMvc.perform(post("/user")
-                .contentType(MediaType.APPLICATION_JSON).requestAttr("userPostRequest", userToAdd));
+        ResponseEntity<String> result = sut.userPost(userToAdd);
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+        assertEquals("Request body is malformed", result.getBody());
+    }
 
-        // Assert
-        result.andExpect(status().isBadRequest()).andExpect(content().string("Request body is malformed"));
+    @Test
+    void registerEmailAlreadyExistsInput(){
+        UserPostRequest userToAddSameEmail = new UserPostRequest("user2","iexisttwice@gmail.com","pass");
+
+        ResponseEntity<String> result = sut.userPost(userToAddSameEmail);
+        assertEquals(HttpStatus.CONFLICT, result.getStatusCode());
+        assertEquals("User with email already exists", result.getBody());
+    }
+
+    @Test
+    void registerInvalidUsername(){
+        UserPostRequest userToAdd = new UserPostRequest("!user","email@gmail.com","pass123");
+
+        ResponseEntity<String> result = sut.userPost(userToAdd);
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+        assertEquals("Username or email format was incorrect", result.getBody());
+    }
+
+    @Test
+    void registerDatabaseFailed(){
+        UserPostRequest userToAdd = new UserPostRequest("userImpossible","email@gmail.com","pass123");
+
+        ResponseEntity<String> result = sut.userPost(userToAdd);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatusCode());
+        assertEquals("Database insertion failed", result.getBody());
+    }
+
+    @Test
+    void registerOk(){
+        UserPostRequest userToAdd = new UserPostRequest("user","email@gmail.com","pass123");
+
+        ResponseEntity<String> result = sut.userPost(userToAdd);
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals("User created successfully", result.getBody());
     }
 }
