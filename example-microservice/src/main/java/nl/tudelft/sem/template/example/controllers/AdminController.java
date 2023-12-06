5f5aca9baa9222613ef97b4c21e2c3fb2fdca5a0
baturalpkarslioglu <B.Karsloglu@tudelft.nl>
2023-12-06 18:58:39 +0100

    Added the admin controller.

---
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/AdminController.java
@@ -0,0 +1,107 @@
+package nl.tudelft.sem.template.example.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+
+@RestController
+public class AdminController {
+
+    /**
+     * POST /admin/{adminID}/addBook : Adds a book to the book repository
+     *
+     * @param adminID Numeric ID of the admin that makes the request (required)
+     * @param book  (required)
+     * @return Book added successfully (status code 200)
+     *         or User does not have admin privileges (status code 401)
+     *         or Book could not be added (status code 500)
+     */
+    @Operation(
+            operationId = "adminAdminIDAddBookPost",
+            summary = "Adds a book to the book repository",
+            tags = { "Admin Operations" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Book added successfully"),
+                    @ApiResponse(responseCode = "401", description = "User does not have admin privileges"),
+                    @ApiResponse(responseCode = "500", description = "Book could not be added")
+            }
+    )
+    @PostMapping("/admin/{adminID}/addBook")
+    public ResponseEntity<Void> adminAdminIDAddBookPost(
+            @Parameter(name = "adminID", required = true) @PathVariable("adminID") Integer adminID,
+            @Parameter(name = "Book", required = true) @Valid @RequestBody Book book
+    ) {
+        int id = adminID;
+
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+
+    }
+
+    /**
+     * DELETE /admin/{adminID}/removeBook/{bookID} : Removes a book from the book repository
+     *
+     * @param adminID Numeric ID of the admin that makes the request (required)
+     * @param bookID ID of the book to delete (required)
+     * @return Book removed successfully (status code 200)
+     *         or User does not have admin privileges (status code 401)
+     *         or Book could not be found (status code 404)
+     *         or Book could not be removed (status code 500)
+     */
+    @Operation(
+            operationId = "adminAdminIDRemoveBookBookIDDelete",
+            summary = "Removes a book from the book repository",
+            tags = { "Admin Operations" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Book removed successfully"),
+                    @ApiResponse(responseCode = "401", description = "User does not have admin privileges"),
+                    @ApiResponse(responseCode = "404", description = "Book could not be found"),
+                    @ApiResponse(responseCode = "500", description = "Book could not be removed")
+            }
+    )
+    @DeleteMapping("/admin/{adminID}/removeBook/{bookID}")
+    public ResponseEntity<Void> adminAdminIDRemoveBookBookIDDelete(
+            @Parameter(name = "adminID", required = true) @PathVariable("adminID") Integer adminID,
+            @Parameter(name = "bookID", required = true) @PathVariable("bookID") Integer bookID
+    ) {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+
+    }
+
+    /**
+     * PUT /admin/{adminID}/editBook/{bookID} : Edits a book from the book repository
+     *
+     * @param adminID Numeric ID of the admin that makes the request (required)
+     * @param bookID ID of the book to update (required)
+     * @param book  (required)
+     * @return Book edits saved successfully (status code 200)
+     *         or User does not have admin privileges (status code 401)
+     *         or Book could not be found (status code 404)
+     *         or Book could not be edited (status code 500)
+     */
+    @Operation(
+            operationId = "adminAdminIDEditBookBookIDPut",
+            summary = "Edits a book from the book repository",
+            tags = { "Admin Operations" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Book edits saved successfully"),
+                    @ApiResponse(responseCode = "401", description = "User does not have admin privileges"),
+                    @ApiResponse(responseCode = "404", description = "Book could not be found"),
+                    @ApiResponse(responseCode = "500", description = "Book could not be edited")
+            }
+    )
+    @PutMapping("/admin/{adminID}/editBook/{bookID}")
+    public ResponseEntity<Void> adminAdminIDEditBookBookIDPut(
+            @Parameter(name = "adminID", required = true) @PathVariable("adminID") Integer adminID,
+            @Parameter(name = "bookID", required = true) @PathVariable("bookID") Integer bookID,
+            @Parameter(name = "Book", required = true) @Valid @RequestBody Book book
+    ) {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+
+    }
+}
