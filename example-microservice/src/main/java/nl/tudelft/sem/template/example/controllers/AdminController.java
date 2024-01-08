3633d4f77da85300161281921d6f97dec6124c8d
baturalpkarslioglu <B.Karsloglu@tudelft.nl>
2024-01-08 15:26:08 +0100

    Added the admin controller for the book modificaiton.

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/AdminController.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/AdminController.java
@@ -3,6 +3,7 @@ package nl.tudelft.sem.template.example.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import nl.tudelft.sem.template.example.domain.book.Book;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
